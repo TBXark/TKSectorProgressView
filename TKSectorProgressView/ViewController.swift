cbd8b59d2917b13b2fdc11a0fea53369220e7dce
TBXark <vfanx@qq.com>
2016-08-04 14:16:05 +0800

    Initial Commit

---
+++ TKSectorProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TKSectorProgressView
+//
+//  Created by Tbxark on 8/4/16.
+//  Copyright © 2016 Tbxark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
