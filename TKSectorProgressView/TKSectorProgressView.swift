c60ab07fb731ddf12514ca72ab16c663e6e3c396
TBXark <vfanx@qq.com>
2016-08-04 20:09:48 +0800

    project init

---
+++ TKSectorProgressView/TKSectorProgressView.swift
@@ -0,0 +1,104 @@
+//
+//  TKSectorProgressView.swift
+//  TKSectorProgressView
+//
+//  Created by Tbxark on 8/4/16.
+//  Copyright © 2016 Tbxark. All rights reserved.
+//
+
+import UIKit
+
+
+struct TKSectorProgressViewConfigure {
+    var borderColor: UIColor = UIColor(red:0.862,  green:0.272,  blue:0.282, alpha:1)
+    var fillColor: UIColor = UIColor.whiteColor()
+    var sectorColor: UIColor = UIColor(red:1,  green:0.493,  blue:0.474, alpha:1)
+    var borderWidth: CGFloat = 0.5
+    var innerInset: CGFloat = 5
+}
+
+
+class TKSectorProgressView: UIView {
+    
+    var progress: CGFloat = 0 {
+        didSet {
+            if progress != oldValue {
+                setNeedsDisplay()
+            }
+        }
+    }
+    var configure = TKSectorProgressViewConfigure()
+    private var targetProgress: CGFloat?
+    private var progressStep: CGFloat = 1
+    private var currentLink: CADisplayLink?
+    var animateDuration: NSTimeInterval = 2.0
+
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clearColor()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = UIColor.clearColor()
+
+    }
+    
+    
+    
+    func setProgress(progress: CGFloat, animate: Bool) {
+        if animate {
+            currentLink?.invalidate()
+            targetProgress = progress
+            let link = CADisplayLink(target: self, selector: #selector(TKSectorProgressView.changeProgress))
+            link.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+            progressStep = (progress - self.progress)/CGFloat(animateDuration * 60 / NSTimeInterval(link.frameInterval))
+            currentLink = link
+        } else {
+            self.progress = progress
+        }
+    }
+    
+    @objc func changeProgress() {
+        guard let target = targetProgress else { return }
+        let newProgress = progressStep + progress
+        progress = newProgress
+        if abs(target - progress) < 0.000001 {
+            progress = target
+            currentLink?.invalidate()
+            targetProgress = nil
+        }
+    }
+    
+    
+    override func drawRect(rect: CGRect) {
+        let radius = min(rect.width, rect.height) - configure.borderWidth
+        let pRect = CGRect(x: (rect.width - radius)/2,
+                           y: (rect.height - radius)/2,
+                           width: radius,
+                           height: radius)
+        let back = UIBezierPath(ovalInRect: pRect)
+        configure.borderColor.setStroke()
+        configure.fillColor.setFill()
+        back.lineWidth = configure.borderWidth
+        back.fill()
+        back.stroke()
+        
+        let center = CGPoint(x: rect.width/2, y: rect.height/2)
+        let startAngle = CGFloat(-M_PI/2)
+        let endAngle = min(progress, 1) * CGFloat(M_PI * 2) + startAngle
+        let sectorPath = UIBezierPath(arcCenter: center,
+                                      radius: radius/2 - configure.borderWidth/2 - configure.innerInset,
+                                      startAngle: startAngle,
+                                      endAngle: endAngle,
+                                      clockwise: true)
+        sectorPath.addLineToPoint(center)
+        sectorPath.closePath()
+        configure.sectorColor.setFill()
+        sectorPath.fill()
+    }
+}
--- TKSectorProgressView/ViewController.swift
+++ TKSectorProgressView/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    private let progress = TKSectorProgressView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        progress.center = view.center
+        
+        var rect = progress.frame
+        rect.origin.y += rect.height + 20
+        rect.size.height = 30
+        let control = UISlider(frame: rect)
+        
+        view.addSubview(progress)
+        view.addSubview(control)
+        progress.configure.borderWidth = 5
+//        progress.setProgress(1, animate: true) // Change progress with animation
+        control.tintColor = progress.configure.borderColor
+        control.addTarget(self, action: #selector(ViewController.changrProgress(_:)), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func changrProgress(slider: UISlider) {
+        progress.progress = CGFloat(slider.value)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
