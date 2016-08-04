88eb97f7d82c83d33e307806a1c257ed960d7f0f
TBXark <vfanx@qq.com>
2016-08-04 21:11:16 +0800

    fix carthage

---
+++ TKSectorProgressView iOS/TKSectorProgressView iOS.h
@@ -0,0 +1,19 @@
+//
+//  TKSectorProgressView iOS.h
+//  TKSectorProgressView iOS
+//
+//  Created by Tbxark on 8/4/16.
+//  Copyright © 2016 Tbxark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TKSectorProgressView iOS.
+FOUNDATION_EXPORT double TKSectorProgressView_iOSVersionNumber;
+
+//! Project version string for TKSectorProgressView iOS.
+FOUNDATION_EXPORT const unsigned char TKSectorProgressView_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TKSectorProgressView_iOS/PublicHeader.h>
+
+
